c370c97dc5db2d48666b6c6a8c610142e3450b0c
toeger <toeger@anonymouse.me>
2016-04-28 20:38:19 +0200

    logging

---
+++ log.h
@@ -0,0 +1,58 @@
+#ifndef LOG_H
+#define LOG_H
+
+#define LOG_LEVEL_NONE 0
+#define LOG_LEVEL_NOTE 1
+#define LOG_LEVEL_DEBUG 2
+#define LOG_LEVEL_ALL 3
+
+#define LOG_LEVEL_DEFAULT LOG_LEVEL_ALL
+
+#define LOG_LEVEL LOG_LEVEL_DEFAULT
+
+#include <iostream>
+#include <functional>
+
+namespace Log
+{
+	struct Log_dummy{
+		template<class T>
+		Log_dummy &operator <<(T&&){
+			return *this;
+		}
+	};
+	struct Log_real{
+		template<class T>
+		Log_real &operator <<(T&&t){
+			std::cerr << std::forward<T>(t);
+			return *this;
+		}
+		~Log_real(){
+			std::cerr << std::endl;
+		}
+	};
+
+	inline auto log_note(){
+#if LOG_LEVEL >= 3
+		return Log_real{};
+#else
+		return Log_dummy{};
+#endif
+	}
+	inline auto log_debug(){
+#if LOG_LEVEL >= 2
+		return Log_real{};
+#else
+		return Log_dummy{};
+#endif
+	}
+	inline auto log_all(){
+#if LOG_LEVEL >= 1
+		return Log_real{};
+#else
+		return Log_dummy{};
+#endif
+	}
+};
+
+#endif // LOG_H
