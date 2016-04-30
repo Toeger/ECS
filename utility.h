e57076ec156acfb2cd280cb16f4d78b9f38dfe1b
toeger <toeger@anonymouse.me>
2016-04-30 20:14:15 +0200

    added Move_only_pointer, not sure if that is actually needed

--- utility.h
+++ utility.h
@@ -41,13 +41,44 @@ namespace Utility {
 			std::move(f)();
 		}
 
-			private:
+		private:
 		Function f;
 	};
 	template <class Function>
 	RAII<Function> create_RAII(Function &&f) {
 		return RAII<remove_cvr<Function>>(std::forward<Function>(f));
 	}
+	template <class T>
+	struct Move_only_pointer {
+		Move_only_pointer()
+			: t(nullptr) {}
+		Move_only_pointer(T *t)
+			: t(t) {}
+		Move_only_pointer(Move_only_pointer &&other)
+			: t(other.t) {
+			other.t = nullptr;
+		}
+		Move_only_pointer(const Move_only_pointer &other) = delete;
+		Move_only_pointer &operator=(Move_only_pointer &&other) {
+			std::swap(t, other.t);
+			return *this;
+		}
+		Move_only_pointer &operator=(const Move_only_pointer &other) = delete;
+		Move_only_pointer &operator=(T other) {
+			t = other;
+			return *this;
+		}
+		T *operator->() const {
+			return t;
+		}
+		operator bool() const {
+			return t;
+		}
+
+		private:
+		T *t;
+	};
+
 #define ON_SCOPE_EXIT_CAT(a, b) ON_SCOPE_EXIT_CAT_(a, b) // force expand
 #define ON_SCOPE_EXIT_CAT_(a, b) a##b                    // actually concatenate
 #define ON_SCOPE_EXIT(CODE) auto ON_SCOPE_EXIT_CAT(ON_SCOPE_EXIT_, __LINE__) = Utility::create_RAII([&]() { CODE })
